5f94a191f1bfe239793f3967e0f2ee6b7949e279
Nisar Ahmed <nisarahmed@Nisars-MacBook-Pro.local>
2022-05-10 00:04:01 -0500

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 Demo Mongo Java Interactions
 
+This is a demo api which uses Java Springboot and MongoDB using student log application.
+
+Api end points:
+@GetMapping
+Get List of all Students from MongoDB
+localhost:8080/api/v1/listallstudents
+
+@PostMapping
+Add one student
+localhost:8080/api/v1/addstudent
+
+Sampele Student Json:
+
+{
+"firstName": "Jane",
+"lastName": "Dough",
+"email": "jane-dough@gmail.com",
+"age": 22,
+"gender": "Female",
+"address": {
+"city": "Hopkins",
+"zipcode": 12345,
+"country": "USA"
+},
+"subjects": ["Maths", "Science", "English"],
+"fees": 2000
+}
--- src/main/java/com/demo_mongo_java_api/controller/StudentRestController.java
+++ src/main/java/com/demo_mongo_java_api/controller/StudentRestController.java
@@ -6,10 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,25 +15,25 @@ public class StudentRestController {
     @Autowired
     MongoRepository repository;
 
-    @GetMapping("allstudents")
+    @GetMapping("/api/v1/listallstudents")
     public ResponseEntity<List<Student>> getStudents() {
 
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
 
     }
 
-    @PostMapping(path = "addstudent",
+    @PostMapping(path = "/api/v1/addstudent",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Student> create(@RequestBody Student student) throws Exception {
+    public ResponseEntity<Student> create(@RequestBody Student student) {
         repository.insert(student);
-        if (student == null) {
-            throw new Exception("Empty User");
-        } else {
             return new ResponseEntity<>(student, HttpStatus.CREATED);
         }
     }
 
 
 
-}
+
+
+
+
